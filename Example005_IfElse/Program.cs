f7efcb5cb7a985872741b79106854f69121acf62
Rayenire <aeristarg@gmail.com>
2022-06-24 13:11:16 +0300

    Example005

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алёна")
+{
+    Console.WriteLine("Ура, это же АЛЁНА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
